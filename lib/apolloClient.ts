d0bd0a294c9e6c411a04272d96e8fdc869d26836
gbauh <gaboumorales@gmail.com>
2022-10-18 12:43:09 -0500

    modified link

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const link = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_WORDPRESS_API_URL,
+  uri: `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/graphql`,
   credentials: 'include',
 });
 
